879d51fcd0d76301a4d42b80900f8ebd9b05868c
carmenluo <Sdluojm@gmail.com>
2019-10-09 23:43:13 -0400

    set up websocket

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,7 @@ import Loading from 'components/Loading';
 import Panel from 'components/Panel';
 import classnames from "classnames";
 import axios from "axios";
+import { setInterview } from "helpers/reducers";
 import {
   getTotalInterviews,
   getLeastPopularTimeSlot,
@@ -61,6 +62,16 @@ class Dashboard extends Component {
         interviewers: interviewers.data
       });
     });
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    this.socket.onmessage = event => {
+      const data = JSON.parse(event.data);
+    
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState(previousState =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
     if (focused) {
       this.setState({ focused });
     }
@@ -71,7 +82,9 @@ class Dashboard extends Component {
       localStorage.setItem("focused", JSON.stringify(this.state.focused));
     }
   }
-
+  componentWillUnmount() {
+    this.socket.close();
+  }
   render() {
 
     const dashboardClasses = classnames("dashboard", { "dashboard-focused": this.state.focused });
