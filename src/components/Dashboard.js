4edd5f26de52adf969b0a5938b43eb178532db96
carmenluo <Sdluojm@gmail.com>
2019-10-09 22:37:25 -0400

    add loading page and panels with fake data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,43 @@
 import React, { Component } from "react";
-
+import Loading from 'components/Loading';
+import Panel from 'components/Panel';
 import classnames from "classnames";
-
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
   render() {
     const dashboardClasses = classnames("dashboard");
-
-    return <main className={dashboardClasses} />;
+    let panels = data.map(item => <Panel key={item.id} id = {item.id} label= {item.label} value ={item.value}/>);
+    if (this.state.loading) {
+      return <Loading />;
+    }
+    return <main className={dashboardClasses}>{panels}</main>
+    
   }
 }
 
 export default Dashboard;
+      
